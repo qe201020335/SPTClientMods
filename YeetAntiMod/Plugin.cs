c27274595e436104f534d8c037fe421c99cb0349
qe201020335 <qe201020335@sina.com>
2024-02-17 18:23:46 -0500

    yeet aki's bleeding edge anti-mods

---
+++ YeetAntiMod/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using BepInEx;
+
+namespace YeetAntiMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        new AllowClientModsPatch().Enable();
+    }
+}
+
+public class AllowClientModsPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(Aki.Core.Patches.PreventClientModsPatch).GetMethod("CheckForNonWhitelistedPlugins", BindingFlags.Static | BindingFlags.NonPublic);
+    }
+        
+    [PatchPrefix]
+    private static bool PatchPreFix()
+    {
+        return false;  // skip it
+    }
+        
+    // [PatchTranspiler]
+    // private IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    // {
+    //     Debug.LogError("Transpiling Anti-Mods");
+    //     foreach (var instruction in instructions)
+    //     {
+    //         if (instruction.opcode == OpCodes.Throw)
+    //         {
+    //             yield return new CodeInstruction(OpCodes.Nop);
+    //         }
+    //         else
+    //         {
+    //             yield return instruction;
+    //         }
+    //     }
+    // }
+}
